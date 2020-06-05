6bdedef48bd11e854788c12ee6f711cd2c3caca6
Matthew <jiannaz1@uci.edu>
2020-06-04 20:56:35 -0700

    add git Auth

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 ## Q & A community
 
 ## info
-https://spring.io/guides/gs/serving-web-content/
+[spring doc]https://spring.io/guides/gs/serving-web-content/
+[Github OAuth]https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/
 
 ## tools
 git
---
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -0,0 +1,30 @@
+package life.majiang.community.controller;
+
+import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.dto.GithubUser;
+import life.majiang.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("Iv1.48fbd836949e4a44");
+        accessTokenDTO.setClient_secret("62cc214a16528246d21f5756a87a935d27a4a3a1");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_url("http://localhost:8887/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
